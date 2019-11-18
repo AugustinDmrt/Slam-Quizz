ed1aa9390100c58c3f9cdbd7c8fd3a4ea7a28c76
damoaug <damoaug@ndlp.fr>
2019-11-18 10:34:46 +0100

    [Core] Add PostgreSql config #3

--- src/Migrations/Version20191118091930.php
+++ src/Migrations/Version20191118091930.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191114161420 extends AbstractMigration
+final class Version20191118091930 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,9 @@ final class Version20191114161420 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SEQUENCE tbl_category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE tbl_category (id INT NOT NULL, shortname VARCHAR(50) NOT NULL, longname VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE SEQUENCE tbl_user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE tbl_user (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_38B383A1E7927C74 ON tbl_user (email)');
     }
 
     public function down(Schema $schema) : void
@@ -32,7 +33,7 @@ final class Version20191114161420 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE tbl_category_id_seq CASCADE');
-        $this->addSql('DROP TABLE tbl_category');
+        $this->addSql('DROP SEQUENCE tbl_user_id_seq CASCADE');
+        $this->addSql('DROP TABLE tbl_user');
     }
 }
