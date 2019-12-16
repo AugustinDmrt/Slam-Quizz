477a19520379c4c93e02c155b18855fb6c230283
damoaug <damoaug@ndlp.fr>
2019-12-16 11:00:04 +0100

    [Core] Add Question entity & CRUD #9

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class Category
      * @ORM\Column(type="string", length=255)
      */
     private $longname;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Question", mappedBy="categories")
+     */
+    private $questions;
+
+    public function __construct()
+    {
+        $this->questions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +64,34 @@ class Category
     public function setLongname(string $longname): self
     {
         $this->longname = $longname;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Question[]
+     */
+    public function getQuestions(): Collection
+    {
+        return $this->questions;
+    }
+
+    public function addQuestion(Question $question): self
+    {
+        if (!$this->questions->contains($question)) {
+            $this->questions[] = $question;
+            $question->addCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuestion(Question $question): self
+    {
+        if ($this->questions->contains($question)) {
+            $this->questions->removeElement($question);
+            $question->removeCategory($this);
+        }
 
         return $this;
     }
